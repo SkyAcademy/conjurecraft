974bc00740935220a1d919888606ee5dfbcc4716
ConjureCraft <42539420+SkyAcademy@users.noreply.github.com>
2019-05-01 14:29:16 +0200

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,10 @@
+<?php include "includes/menu.php";?>
+
+
+<div id='content'>
+<h1><strong>Welkom <?php echo $_SESSION['username']; ?></strong></h1>
+<p></p>
+</div>
+
+
+<?php include "includes/foot.php";?>
